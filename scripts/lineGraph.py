80b63c31f8155b749dfd0e699c24c9231dd41bb8
Tom Gaillard <LE1826622@crc-lennox.qc.ca>
2022-04-19 09:54:28 -0400

    Added argv and connection to DB

---
+++ scripts/lineGraph.py
@@ -0,0 +1,40 @@
+import sys 
+from pydoc import doc
+import pymongo
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+FILTER = sys.argv[1]
+
+
+def ConnToDb():
+    myClient = pymongo.MongoClient(
+        "mongodb://cfortier:cfortier123@cluster0-shard-00-00.gjdrt.mongodb.net:27017,cluster0-shard-00-01.gjdrt.mongodb.net:27017,cluster0-shard-00-02.gjdrt.mongodb.net:27017/test?authSource=admin&replicaSet=atlas-e0cio3-shard-0&readPreference=primary&ssl=true"
+    )
+    mydb = myClient["sample_supplies"]
+
+    collection = mydb["sales"]
+    return collection
+
+
+
+def FetchData():
+    coll = ConnToDb()
+    # df = pd.DataFrame(coll)
+    doc = list(
+        coll.aggregate([{"$match": {"storeLocation": FILTER}}])
+    )
+    return doc
+
+
+def main(): 
+    print(sys.argv[1])
+    ConnToDb()
+    coll = FetchData()
+    salesDF = pd.DataFrame(coll)
+    print(salesDF)
+
+if __name__ == "__main__":
+    main()
