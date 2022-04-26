56fafe316d1a64d32497799ea3a250f54d8bbdb7
Sacha <90349728+SachaPaquette@users.noreply.github.com>
2022-04-26 18:09:57 -0400

    upload route

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import Home from './views/Home';
 import Register from './views/Register';
 import Login from './views/Login';
+import Upload from './views/Upload';
 import NotFound from './views/NotFound';
 import Protected from './views/Protected';
 
@@ -21,6 +22,7 @@ export default function App() {
             <Route path='register' element={<Register/>}/>
             <Route path="login" element={<Login />} />
             <Route path="protected" element={<Protected />} />
+            <Route path="upload" element={<Upload />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
